258f4f345e4443b22aff5d73de3cc00c08049f00
Luke Carl Thompson <lukecarlthompson@gmail.com>
2025-12-06 17:39:19 +0800

    update examples

--- src/examples/PixiCanvas.example.tsx
+++ src/examples/PixiCanvas.example.tsx
@@ -4,7 +4,7 @@ import { PixiApplication, PixiCanvas, PixiStage, Sprite } from "pixi-solid";
 export const PixiCanvasExample = () => {
   return (
     <PixiApplication>
-      <PixiCanvas>
+      <PixiCanvas style={{ background: "#1099bb" }}>
         <PixiStage>
           <Sprite texture={Texture.WHITE} x={100} y={100} />
         </PixiStage>
