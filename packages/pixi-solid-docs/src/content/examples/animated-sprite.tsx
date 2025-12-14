808429e3cc40decbf66aa3b5a708e6ad2c987fd7
Luke Carl Thompson <lukecarlthompson@gmail.com>
2025-12-14 20:00:13 +0800

    update examples

--- packages/pixi-solid-docs/src/content/examples/animated-sprite.tsx
+++ packages/pixi-solid-docs/src/content/examples/animated-sprite.tsx
@@ -2,22 +2,20 @@ import type * as Pixi from "pixi.js";
 import { Assets } from "pixi.js";
 import { AnimatedSprite } from "pixi-solid";
 
-export const RunningMan = (props: Pixi.ContainerOptions) => {
-  const textures = Object.values(
+export const DemoComponent = () => {
+  // Get pre-loaded textures and convert to an array
+  const runTextures = Object.values(
     Assets.get<Pixi.Texture>(["/run_0.png", "/run_1.png", "/run_2.png", "/run_3.png", "/run_4.png", "/run_5.png"])
   );
 
   return (
     <AnimatedSprite
-      ref={(animatedSprite) => {
-        animatedSprite.play();
-      }}
-      textures={textures}
+      autoPlay={true}
+      textures={runTextures}
       scale={4}
       animationSpeed={0.25}
       anchor={{ x: 0.5, y: 0.5 }}
       position={{ x: 350, y: 300 }}
-      {...props}
     />
   );
 };
