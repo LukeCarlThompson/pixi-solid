3d7a5b627daf82eb432df45e98f60bc554adb3ea
Luke Carl Thompson <lukecarlthompson@gmail.com>
2025-12-14 20:00:13 +0800

    update examples

--- packages/pixi-solid-docs/src/content/examples/sprite.demo.tsx
+++ packages/pixi-solid-docs/src/content/examples/sprite.demo.tsx
@@ -1,14 +1,25 @@
-import { PixiApplication, PixiCanvas, PixiStage } from "pixi-solid";
-import { Sky } from "./sprite";
+import type * as Pixi from "pixi.js";
+import { Assets } from "pixi.js";
+import { PixiApplication, PixiCanvas, PixiStage, Sprite } from "pixi-solid";
+import { createResource, Show, Suspense } from "solid-js";
 
 export const DemoApp = () => {
+  // Create a resource to load the sky texture
+  const [textureResource] = createResource(() => Assets.load<Pixi.Texture>("/sky.png"));
   return (
     <PixiApplication background="#1099bb">
-      <PixiCanvas>
-        <PixiStage>
-          <Sky />
-        </PixiStage>
-      </PixiCanvas>
+      <Suspense fallback={<div>Loading...</div>}>
+        <PixiCanvas style={{ height: "600px" }}>
+          {/* Show our Stage when the assets are loaded */}
+          <Show when={textureResource()}>
+            {(texture) => (
+              <PixiStage>
+                <Sprite texture={texture()} position={{ x: 0, y: -50 }} scale={3.6} />
+              </PixiStage>
+            )}
+          </Show>
+        </PixiCanvas>
+      </Suspense>
     </PixiApplication>
   );
 };
--- packages/pixi-solid-docs/src/content/examples/tiling-sprite.demo.tsx
+++ packages/pixi-solid-docs/src/content/examples/tiling-sprite.demo.tsx
@@ -1,17 +1,17 @@
 import type * as Pixi from "pixi.js";
 import { Assets, TextureStyle } from "pixi.js";
-import { PixiApplication, PixiCanvas, PixiStage, Sprite } from "pixi-solid";
-import { createResource, createSignal, For, Show } from "solid-js";
-import { RunningMan } from "./animated-sprite";
-import { Sky } from "./sprite";
-import { Ground } from "./tiling-sprite";
+import { AnimatedSprite, PixiApplication, PixiCanvas, PixiStage, Sprite, TilingSprite, useTick } from "pixi-solid";
+import { createResource, Show } from "solid-js";
 
 export const DemoApp = () => {
-  const [texturesResource] = createResource(async () => {
-    Assets.init();
+  // Create a resource to load the sky texture
+  const [textureResource] = createResource(async () => {
     // Setting scale mode to nearest for crisp pixel art
     TextureStyle.defaultOptions.scaleMode = "nearest";
-    const assets = await Assets.load<Pixi.Texture>([
+
+    const skyTexture = await Assets.load<Pixi.Texture>("/sky.png");
+    const groundTexture = await Assets.load<Pixi.Texture>("/ground-tile.png");
+    const runTextures = await Assets.load<Pixi.Texture>([
       "/run_0.png",
       "/run_1.png",
       "/run_2.png",
@@ -20,20 +20,41 @@ export const DemoApp = () => {
       "/run_5.png",
     ]);
 
-    const textures = Object.values(assets);
-
-    return textures;
+    return { skyTexture, groundTexture, runTextures: Object.values(runTextures) };
   });
-
   return (
     <PixiApplication background="#1099bb">
       <PixiCanvas>
-        <Show when={texturesResource()}>
-          <PixiStage>
-            <Sky />
-            <Ground />
-            <RunningMan />
-          </PixiStage>
+        {/* Show our Stage when the assets are loaded */}
+        <Show when={textureResource()}>
+          {(textures) => (
+            <PixiStage>
+              <Sprite texture={textures().skyTexture} position={{ x: 0, y: -50 }} scale={3.6} />
+              <TilingSprite
+                ref={(tileRef) => {
+                  useTick((ticker) => {
+                    tileRef.tilePosition.x -= 6 * ticker.deltaTime;
+                  });
+                }}
+                texture={textures().groundTexture}
+                width={800}
+                height={200}
+                tileScale={{ x: 5, y: 5 }}
+                tilePosition={{ x: 0, y: 0 }}
+                position={{ x: 0, y: 400 }}
+              />
+              <AnimatedSprite
+                ref={(animatedSprite) => {
+                  animatedSprite.play();
+                }}
+                textures={textures().runTextures}
+                scale={4}
+                animationSpeed={0.25}
+                anchor={{ x: 0.5, y: 0.5 }}
+                position={{ x: 350, y: 300 }}
+              />
+            </PixiStage>
+          )}
         </Show>
       </PixiCanvas>
     </PixiApplication>
