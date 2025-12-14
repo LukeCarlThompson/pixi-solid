a0e03b693c917b81a79aef034e22c2c8d7641be6
Luke Carl Thompson <lukecarlthompson@gmail.com>
2025-12-14 19:57:41 +0800

    add in events page

---
+++ packages/pixi-solid-docs/src/content/docs/events/event-names-as-string.tsx
@@ -0,0 +1,19 @@
+import { createResource, For, Show } from "solid-js";
+
+export const EventNamesList = () => {
+  const [eventNamesResource] = createResource(async () => {
+    const eventNames = await import("pixi-solid").then((mod) => mod.PIXI_EVENT_HANDLER_NAMES);
+
+    return eventNames;
+  });
+
+  return (
+    <Show when={eventNamesResource()}>
+      {(eventNames) => (
+        <ul>
+          <For each={eventNames()}>{(eventName) => <li>{eventName}</li>}</For>
+        </ul>
+      )}
+    </Show>
+  );
+};
