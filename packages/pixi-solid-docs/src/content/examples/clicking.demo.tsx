3ae8cdd37a97c0baca20584f83dbe22517da2f39
Luke Carl Thompson <lukecarlthompson@gmail.com>
2026-01-25 12:24:32 +0800

    update docs

---
+++ packages/pixi-solid-docs/src/content/examples/clicking.demo.tsx
@@ -0,0 +1,64 @@
+import { DEG_TO_RAD } from "pixi.js";
+import { Graphics, PixiApplication, PixiCanvas, PixiStage, Text, usePixiScreen } from "pixi-solid";
+import { createSignal } from "solid-js";
+
+const ClickingDemo = () => {
+  const screen = usePixiScreen();
+  const [numberOfClicks, setNumberOfClicks] = createSignal(0);
+  const textValue = () => `Number of clicks: ${numberOfClicks()}`;
+
+  const handleClick = () => {
+    setNumberOfClicks((prev) => prev + 1);
+  };
+
+  return (
+    <PixiStage>
+      <Text
+        text={textValue()}
+        style={{ fill: "#ffffff", align: "center" }}
+        anchor={0.5}
+        x={screen.width * 0.5}
+        y={screen.height * 0.1}
+      />
+      <Graphics
+        ref={(graphics) => {
+          graphics.roundRect(-70, -30, 140, 60, 8).fill("#0099ff");
+        }}
+        eventMode={"static"}
+        cursor={"pointer"}
+        onclick={handleClick}
+        x={screen.width * 0.5}
+        y={screen.height * 0.5}
+      />
+      <Text
+        text={"Click me!"}
+        style={{
+          fill: "#ffffff",
+          align: "center",
+          fontSize: 18,
+          padding: 5,
+          dropShadow: {
+            alpha: 0.5,
+            blur: 2,
+            distance: 2,
+            angle: 90 * DEG_TO_RAD,
+            color: "#000000",
+          },
+        }}
+        anchor={0.5}
+        x={screen.width * 0.5}
+        y={screen.height * 0.5}
+      />
+    </PixiStage>
+  );
+};
+
+export const DemoApp = () => {
+  return (
+    <PixiApplication antialias={true} resolution={window.devicePixelRatio}>
+      <PixiCanvas style={{ "aspect-ratio": "2/1.5" }}>
+        <ClickingDemo />
+      </PixiCanvas>
+    </PixiApplication>
+  );
+};
---
+++ packages/pixi-solid-docs/src/content/examples/use-spring.demo.tsx
@@ -0,0 +1,94 @@
+import type * as Pixi from "pixi.js";
+import { FillGradient } from "pixi.js";
+import { Graphics, PixiApplication, PixiCanvas, PixiStage } from "pixi-solid";
+import { useSpring } from "pixi-solid/utils";
+import { createSignal } from "solid-js";
+
+const DraggingDemo = () => {
+  const [isDragging, setIsDragging] = createSignal(false);
+  const pointerPosition = { x: 100, y: 100 };
+
+  const draggingScale = 1.4;
+  const droppedScale = 0.8;
+  let scaleTarget = droppedScale;
+
+  const springyScale = useSpring({
+    to: () => scaleTarget,
+    damping: () => (isDragging() ? 50 : 100),
+    stiffness: () => (isDragging() ? 80 : 100),
+    mass: () => 20,
+  });
+
+  const springyPositionX = useSpring({
+    to: () => pointerPosition.x,
+    damping: () => (isDragging() ? 50 : 100),
+    stiffness: () => (isDragging() ? 40 : 100),
+  });
+
+  const springyPositionY = useSpring({
+    to: () => pointerPosition.y,
+    damping: () => (isDragging() ? 60 : 100),
+    stiffness: () => (isDragging() ? 30 : 100),
+  });
+
+  let dragTarget: Pixi.Container | undefined;
+
+  const handlePointerDown = (e: Pixi.FederatedPointerEvent) => {
+    dragTarget = e.target;
+    scaleTarget = draggingScale;
+    setIsDragging(true);
+
+    if (!e.target.parent) return;
+    // Calculate the offset from the dragTarget's local origin to the click point
+    e.target.parent.toLocal(e.global, undefined, pointerPosition);
+  };
+
+  const handlePointerUp = () => {
+    dragTarget = undefined;
+    scaleTarget = droppedScale;
+    setIsDragging(false);
+  };
+
+  const handlePointerMove = (e: Pixi.FederatedPointerEvent) => {
+    if (!dragTarget || !dragTarget.parent) return;
+    dragTarget.parent.toLocal(e.global, undefined, pointerPosition);
+  };
+
+  const gradient = new FillGradient({
+    type: "linear",
+    colorStops: [
+      { offset: 0, color: "#b6e3ff" },
+      { offset: 1, color: "#0099ff" },
+    ],
+  });
+
+  return (
+    <PixiStage>
+      <Graphics
+        ref={(graphics) => {
+          graphics.roundRect(-50, -50, 100, 100, 20).fill(gradient);
+        }}
+        eventMode={"static"}
+        cursor={isDragging() ? "grabbing" : "grab"}
+        onpointerdown={handlePointerDown}
+        onpointerup={handlePointerUp}
+        onpointerupoutside={handlePointerUp}
+        onglobalpointermove={handlePointerMove}
+        angle={springyPositionX.velocity() * 0.05}
+        scale={springyScale.value()}
+        x={springyPositionX.value()}
+        y={springyPositionY.value()}
+      />
+    </PixiStage>
+  );
+};
+
+export const DemoApp = () => {
+  return (
+    <PixiApplication antialias={true} resolution={window.devicePixelRatio}>
+      <PixiCanvas style={{ "aspect-ratio": "2/1.5" }}>
+        <DraggingDemo />
+      </PixiCanvas>
+    </PixiApplication>
+  );
+};
