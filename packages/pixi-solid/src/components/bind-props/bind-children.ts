56104b708bfd679a82f3a695542358611c0d940a
Luke Carl Thompson <lukecarlthompson@gmail.com>
2026-02-13 16:14:29 +0800

    improved error handling

--- packages/pixi-solid/src/components/bind-props/bind-children.ts
+++ packages/pixi-solid/src/components/bind-props/bind-children.ts
@@ -2,6 +2,14 @@ import type * as Pixi from "pixi.js";
 import { children as resolveChildren, createRenderEffect } from "solid-js";
 import type { JSX } from "solid-js";
 
+export class InvalidChildTypeError extends Error {
+  constructor() {
+    super(
+      "Invalid pixi-solid child type. Children must be pixi-solid or PixiJS element. Did you accidentally pass an invalid child to a pixi-solid parent?",
+    );
+  }
+}
+
 export const bindChildrenToContainer = (parent: Pixi.Container, children?: JSX.Element) => {
   const resolvedChildren = resolveChildren(() => children);
 
@@ -12,19 +20,28 @@ export const bindChildrenToContainer = (parent: Pixi.Container, children?: JSX.E
   }
 
   createRenderEffect((prevChildren: Pixi.Container[] | undefined) => {
-    const nextChildren = resolvedChildren.toArray() as unknown as Pixi.Container[];
+    const nextChildren = resolvedChildren.toArray().filter(Boolean) as unknown as Pixi.Container[];
 
-    if (prevChildren) {
-      for (let i = 0; i < prevChildren.length; i += 1) {
-        const child = prevChildren[i];
-        if (nextChildren.includes(child)) continue;
-        parent.removeChild(child);
-        child.destroy({ children: true });
+    try {
+      if (prevChildren) {
+        for (let i = 0; i < prevChildren.length; i += 1) {
+          const child = prevChildren[i];
+          if (nextChildren.includes(child)) continue;
+          parent.removeChild(child);
+          child.destroy({ children: true });
+        }
       }
-    }
 
-    for (let i = 0; i < nextChildren.length; i += 1) {
-      parent.addChildAt(nextChildren[i], i);
+      for (let i = 0; i < nextChildren.length; i += 1) {
+        parent.addChildAt(nextChildren[i], i);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("Invalid children", nextChildren);
+        throw new InvalidChildTypeError();
+      } else {
+        throw error;
+      }
     }
 
     return nextChildren;
@@ -35,19 +52,28 @@ export const bindChildrenToRenderLayer = (parent: Pixi.RenderLayer, children?: J
   const resolvedChildren = resolveChildren(() => children);
 
   createRenderEffect((prevChildren: Pixi.Container[] | undefined) => {
-    const nextChildren = resolvedChildren.toArray() as unknown as Pixi.Container[];
+    const nextChildren = resolvedChildren.toArray().filter(Boolean) as unknown as Pixi.Container[];
 
-    if (prevChildren) {
-      for (let i = 0; i < prevChildren.length; i += 1) {
-        const child = prevChildren[i];
-        if (nextChildren.includes(child)) continue;
+    try {
+      if (prevChildren) {
+        for (let i = 0; i < prevChildren.length; i += 1) {
+          const child = prevChildren[i];
+          if (nextChildren.includes(child)) continue;
 
-        parent.detach(child);
+          parent.detach(child);
+        }
       }
-    }
 
-    for (let i = 0; i < nextChildren.length; i += 1) {
-      parent.attach(nextChildren[i]);
+      for (let i = 0; i < nextChildren.length; i += 1) {
+        parent.attach(nextChildren[i]);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("Invalid children", nextChildren);
+        throw new InvalidChildTypeError();
+      } else {
+        throw error;
+      }
     }
 
     return nextChildren;
