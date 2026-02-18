63d82b3d2164ae8204a2fee04a1456128e058ebd
Luke Carl Thompson <lukecarlthompson@gmail.com>
2026-02-18 09:31:27 +0800

    fix comment

--- packages/pixi-solid/src/testing/index.tsx
+++ packages/pixi-solid/src/testing/index.tsx
@@ -3,7 +3,10 @@ import { children } from "solid-js";
 
 /**
  * This calls the children components but doesn't mount to anything.
- * This is useful for testing components using SollidJS testing library without needing to worry about the Pixi Application or rendering at all. It can be used as a wrapper around the component you want to test in the render function of the testing library.
+ * This is useful for testing components using SolidJS testing library
+ * without needing to worry about the Pixi Application or rendering at all.
+ * It can be used as a wrapper around the component you want to test in
+ * the render function of the testing library.
  */
 export const NoMount = (props: ParentProps): JSX.Element => {
   const c = children(() => props.children);
