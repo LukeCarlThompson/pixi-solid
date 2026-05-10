2a3beb012f09b7ed27927e8f3d1520ea6750c15a
Luke Carl Thompson <lukecarlthompson@gmail.com>
2026-05-10 13:05:08 +0800

    add missing export

--- packages/pixi-solid/src/index.ts
+++ packages/pixi-solid/src/index.ts
@@ -17,7 +17,13 @@ export type {
 export { onResize } from "./on-resize";
 export { onTick } from "./on-tick";
 export type { PixiApplicationProps } from "./pixi-application";
-export { getPixiApp, getTicker, PixiApplicationProvider, TickerProvider } from "./pixi-application";
+export {
+  getPixiApp,
+  getTicker,
+  getRenderer,
+  PixiApplicationProvider,
+  TickerProvider,
+} from "./pixi-application";
 export type { PixiCanvasProps } from "./pixi-canvas";
 export { PixiCanvas } from "./pixi-canvas";
 export {
